def8f264827ed7995580493721bfdd2b0ffd5319
Salatchen <pozhivi.s.moe@gmail.com>
2023-03-09 21:09:05 +0600

    initial commit

---
+++ Ex02_Indexes.py
@@ -0,0 +1,17 @@
+# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+from random import randint
+
+def find_indexes(array, minn, maxx):
+    indexes = []
+    for i in range(len(array)):
+        if minn < array[i] < maxx:
+            indexes.append(i)
+    return indexes
+
+array = [randint(-100,100) for i in range(10)]
+print(array)
+
+minn = int(input('Введите нижнюю границу диапазона: '))
+maxx = int(input('Введите верхнюю границу диапазона: '))
+print(find_indexes(array, minn, maxx))
